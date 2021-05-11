ae549405a35f07432a8e40711327ae2575224e49
Andrey <andrey.b.golubev@gmail.com>
2021-05-12 02:15:04 +0500

    Use HashingStream for incremental hashing rather than sign validation preloaded data

---
+++ src/Stream/Base/SignCleaningStream.php
@@ -0,0 +1,77 @@
+<?php
+declare(strict_types=1);
+
+namespace Sunnylion\TestCipher\Stream\Base;
+
+use GuzzleHttp\Psr7\StreamDecoratorTrait;
+use Psr\Http\Message\StreamInterface;
+use RuntimeException;
+
+class SignCleaningStream implements StreamInterface
+{
+    use StreamDecoratorTrait;
+
+    private const CROPPED_MAC_SIZE = Utils::CROPPED_MAC_SIZE;
+
+    private StreamInterface $stream;
+
+    private string $mac;
+
+    public function __construct(StreamInterface $stream)
+    {
+        $this->stream = $stream;
+    }
+
+    public function read($length): string
+    {
+        if ($length === 0 || $this->stream->eof()) {
+            return '';
+        }
+
+        if ($this->mac === '') {
+            $this->initMac();
+        }
+
+        $read = $this->mac . $this->stream->read($length);
+        $this->mac = substr($read, -self::CROPPED_MAC_SIZE);
+
+        return substr($read, 0, -self::CROPPED_MAC_SIZE);
+    }
+
+    public function getMac(): ?string
+    {
+        if ($this->stream->eof()) {
+            return $this->mac;
+        }
+
+        return null;
+    }
+
+    public function getSize(): ?int
+    {
+        $size = $this->stream->getSize();
+        if ($size !== null) {
+            return $size - self::CROPPED_MAC_SIZE;
+        }
+
+        return null;
+    }
+
+    public function seek($offset, $whence = SEEK_SET): void
+    {
+        $this->stream->seek($offset, $whence);
+        try {
+            $this->initMac();
+        } catch (RuntimeException $e) {
+            throw new RuntimeException('Wrong position');
+        }
+    }
+
+    public function initMac(): void
+    {
+        $this->mac = $this->stream->read(self::CROPPED_MAC_SIZE);
+        if (strlen($this->mac) !== self::CROPPED_MAC_SIZE) {
+            throw new RuntimeException('Too short input');
+        }
+    }
+}
--- src/Stream/SignValidationStream.php
+++ src/Stream/SignValidationStream.php
@@ -6,20 +6,52 @@ namespace Sunnylion\TestCipher\Stream;
 
 
 use GuzzleHttp\Psr7\StreamDecoratorTrait;
+use Jsq\EncryptionStreams\HashingStream;
 use Psr\Http\Message\StreamInterface;
-use Sunnylion\TestCipher\Stream\Base\BufferingStream;
+use Sunnylion\TestCipher\Stream\Base\SignCleaningStream;
+use Sunnylion\TestCipher\Stream\Base\Utils;
+use Sunnylion\TestCipher\Stream\Exception\HashValidationException;
 
 class SignValidationStream implements StreamInterface
 {
     use StreamDecoratorTrait;
 
-    private StreamInterface $stream;
+    private const CROPPED_MAC_SIZE = Utils::CROPPED_MAC_SIZE;
+
+    private HashingStream $stream;
+    private SignCleaningStream $signCleaningStream;
 
     public function __construct(StreamInterface $stream, string $macKey)
     {
-        $this->stream = new BufferingStream(
-            $stream,
-            new SignValidationFilter($macKey)
-        );
+        $this->signCleaningStream = new SignCleaningStream($stream);
+        $this->stream = new HashingStream($this->signCleaningStream, $macKey);
+    }
+
+    /**
+     * @throws HashValidationException
+     */
+    public function read($length): string
+    {
+        if ($length === 0 || $this->stream->eof()) {
+            return '';
+        }
+
+        $read = $this->stream->read($length);
+        if ($this->stream->eof()) {
+            $this->validate($this->stream->getHash(), $this->signCleaningStream->getMac());
+        }
+
+        return $read;
+    }
+
+    /**
+     * @throws HashValidationException
+     */
+    private function validate(string $hash, string $actualMacCropped): void
+    {
+        $expectedMacCropped = substr($hash, 0, self::CROPPED_MAC_SIZE);
+        if ($expectedMacCropped !== $actualMacCropped) {
+            throw new HashValidationException('Wrong sign');
+        }
     }
 }
